77b3c820811b4a6755be2a4c1bbbcc7b9b9e84ad
saadthenexus <hasibulkarimsaad@gmail.com>
2024-11-12 16:50:57 +0600

    Created interface Writeable

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public abstract class User {
+    protected String userID;
+    protected String username;
+    protected String email;
+    protected String password;
+    protected String userType;
+
+    public User(String userID, String username, String email, String password, String userType) {
+        this.userID = userID;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public abstract void displayUserInfo();
+}
---
+++ src/Writeable.java
@@ -0,0 +1,3 @@
+public interface Writeable extends Readable {
+    void addUser(String username, String email, String password);
+}
