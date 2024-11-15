6accde42a6f7c77c6f6c8eae845074028c6353be
saadthenexus <hasibulkarimsaad@gmail.com>
2024-11-15 17:06:57 +0600

    Added a new function validate credential()

--- src/src/UserAuthenticator.java
+++ src/src/UserAuthenticator.java
@@ -18,6 +18,21 @@ public class UserAuthenticator {
     public boolean authenticateUser(String userID) {
         return userFileManager.doesUserExist(userID);
     }
+
+    private boolean validateCredentials(String filePath, String username, String password) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                if (userData[1].equals(username) && userData[3].equals(password)) {
+                    return true; 
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+        return false;
+    }
 }
 
 
